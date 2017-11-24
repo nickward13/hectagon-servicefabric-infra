35106ec105fd9be15be1189883b0c4ed9d6de717
nickward13 <nickward@microsoft.com>
2017-11-24 16:31:01 +1100

    reuse existing certificate

--- Deploy-SFCluster.ps1
+++ Deploy-SFCluster.ps1
@@ -7,11 +7,14 @@ $vaultname = "hectagonclusterkeyvault"
 $vaultgroupname="hectagon-shared"
 $subname="$clustername.$clusterloc.cloudapp.azure.com"
 $groupname="hectagon-servicefabric"
+$secretId="https://hectagonclusterkeyvault.vault.azure.net/secrets/hectagon-servicefabric20171124124609/21df9bde46c14a199038fc417a679b80"
 
 # Create the Service Fabric cluster.
-New-AzureRmServiceFabricCluster  -ResourceGroupName $groupname -TemplateFile 'C:\Users\nickw\source\repos\hectagon-servicefabric-infra\cluster.json' -ParameterFile 'C:\Users\nickw\source\repos\hectagon-servicefabric-infra\cluster.parameters.json' -CertificatePassword $certpwd -CertificateOutputFolder $certfolder -KeyVaultName $vaultname -KeyVaultResouceGroupName $vaultgroupname -CertificateSubjectName $subname
+New-AzureRmServiceFabricCluster -ResourceGroupName $groupname -TemplateFile 'C:\Users\nickw\source\repos\hectagon-servicefabric-infra\cluster.json' -ParameterFile 'C:\Users\nickw\source\repos\hectagon-servicefabric-infra\cluster.parameters.json' -SecretIdentifier $secretId
 
-Import-PfxCertificate -Exportable -CertStoreLocation Cert:\CurrentUser\My -FilePath C:\Users\nickw\mycertificates\mysfcluster20170531104310.pfx -Password $certpwd
+# To create a new cluster with a new certificate:
+# New-AzureRmServiceFabricCluster -ResourceGroupName $groupname -TemplateFile 'C:\Users\nickw\source\repos\hectagon-servicefabric-infra\cluster.json' -ParameterFile 'C:\Users\nickw\source\repos\hectagon-servicefabric-infra\cluster.parameters.json' -CertificatePassword $certpwd -CertificateOutputFolder $certfolder -KeyVaultName $vaultname -KeyVaultResouceGroupName $vaultgroupname -CertificateSubjectName $subname
+# Import-PfxCertificate -Exportable -CertStoreLocation Cert:\CurrentUser\My -FilePath C:\Users\nickw\mycertificates\mysfcluster20170531104310.pfx -Password $certpwd
 
 Connect-ServiceFabricCluster -ConnectionEndpoint hectagonsfcluster.westus.cloudapp.azure.com:19000 -KeepAliveIntervalInSec 10 -X509Credential -ServerCertThumbprint F9C4BB1C7B74F60700DAD0A4F1909F83A1535B68 -FindType FindByThumbprint -FindValue F9C4BB1C7B74F60700DAD0A4F1909F83A1535B68 -StoreLocation CurrentUser -StoreName My
 
