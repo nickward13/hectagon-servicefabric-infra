58128d8b59134efb5261374995c85b412b1c2258
nickward13 <nickward@microsoft.com>
2017-11-24 13:02:50 +1100

    added connect to cluster and get health

--- Deploy-SFCluster.ps1
+++ Deploy-SFCluster.ps1
@@ -12,3 +12,7 @@ $groupname="hectagon-servicefabric"
 New-AzureRmServiceFabricCluster  -ResourceGroupName $groupname -TemplateFile 'C:\Users\nickw\source\repos\hectagon-servicefabric-infra\cluster.json' -ParameterFile 'C:\Users\nickw\source\repos\hectagon-servicefabric-infra\cluster.parameters.json' -CertificatePassword $certpwd -CertificateOutputFolder $certfolder -KeyVaultName $vaultname -KeyVaultResouceGroupName $vaultgroupname -CertificateSubjectName $subname
 
 Import-PfxCertificate -Exportable -CertStoreLocation Cert:\CurrentUser\My -FilePath C:\Users\nickw\mycertificates\mysfcluster20170531104310.pfx -Password $certpwd
+
+Connect-ServiceFabricCluster -ConnectionEndpoint hectagonsfcluster.westus.cloudapp.azure.com:19000 -KeepAliveIntervalInSec 10 -X509Credential -ServerCertThumbprint F9C4BB1C7B74F60700DAD0A4F1909F83A1535B68 -FindType FindByThumbprint -FindValue F9C4BB1C7B74F60700DAD0A4F1909F83A1535B68 -StoreLocation CurrentUser -StoreName My
+
+Get-ServiceFabricClusterHealth
